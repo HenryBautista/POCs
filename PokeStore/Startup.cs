ee198dab4854e0f9f54216f298116db8bb487793
Henry Eloy Bautista Coaquira <henrybautistahtt@hotmail.com>
2020-11-20 12:13:17 -0400

    add application context

--- PokeStore/Startup.cs
+++ PokeStore/Startup.cs
@@ -8,6 +8,8 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using PokeStore.Data;
+using Microsoft.EntityFrameworkCore;
 
 namespace PokeStore
 {
@@ -24,6 +26,8 @@ namespace PokeStore
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+            services.AddDbContext<ApplicationContext>(options =>
+                    options.UseSqlite(Configuration.GetConnectionString("ApplicationContext")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
