469f4580b2000ff0e47162ba7604b18f1a904b3e
Henry Eloy Bautista Coaquira <henrybautistahtt@hotmail.com>
2020-11-20 11:31:29 -0400

    add pokemon model fields

--- PokeStore/Models/Pokemon.cs
+++ PokeStore/Models/Pokemon.cs
@@ -2,6 +2,13 @@ namespace PokeStore.Models
 {
     public class Pokemon
     {
+        public int Id { get; set; }
         
+        public string Name { get; set; }
+        
+        public string Type { get; set; }
+
+        public int Level { get; set; }
+
     }
 }
