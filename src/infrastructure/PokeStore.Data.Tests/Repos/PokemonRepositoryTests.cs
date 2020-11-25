112b0c68085bbae597ac7a1f4a99408589bda5b0
Henry Eloy Bautista Coaquira <henrybautistahtt@hotmail.com>
2020-11-25 10:18:10 -0400

    add test project fro repositories

---
+++ src/infrastructure/PokeStore.Data.Tests/Repos/PokemonRepositoryTests.cs
@@ -0,0 +1,17 @@
+using Xunit; 
+
+namespace PokeStore.Data.Tests.Repos
+{
+    public class PokemonRepositoryTests
+    {
+        [Fact]
+        public void TestName()
+        {
+        //Given
+        
+        //When
+        
+        //Then
+        }       
+    }
+}
