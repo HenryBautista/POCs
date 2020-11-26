28d2b9a024b1382636da5a63b800818229a3dfa1
Henry Eloy Bautista Coaquira <henrybautistahtt@hotmail.com>
2020-11-26 11:45:50 -0400

    add new migrations

--- src/infrastructure/PokeStore.Data/Migrations/20201126141649_enumtype.cs
+++ src/infrastructure/PokeStore.Data/Migrations/20201126141649_enumtype.cs
@@ -1,8 +1,8 @@
 ﻿using Microsoft.EntityFrameworkCore.Migrations;
 
-namespace PokeStore.Web.Migrations
+namespace PokeStore.Data.Migrations
 {
-    public partial class InitialCreate : Migration
+    public partial class enumtype : Migration
     {
         protected override void Up(MigrationBuilder migrationBuilder)
         {
@@ -13,7 +13,7 @@ namespace PokeStore.Web.Migrations
                     Id = table.Column<int>(nullable: false)
                         .Annotation("Sqlite:Autoincrement", true),
                     Name = table.Column<string>(nullable: true),
-                    Type = table.Column<string>(nullable: true),
+                    Type = table.Column<int>(nullable: false),
                     Level = table.Column<int>(nullable: false),
                     PhotoUrl = table.Column<string>(nullable: true)
                 },
