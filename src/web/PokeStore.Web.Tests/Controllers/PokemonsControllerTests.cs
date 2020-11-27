1f36e33bf1faa5e620e6c57353b75978523e6799
Henry Eloy Bautista Coaquira <henrybautistahtt@hotmail.com>
2020-11-27 15:26:55 -0400

    add modification tests

--- src/web/PokeStore.Web.Tests/Controllers/PokemonsControllerTests.cs
+++ src/web/PokeStore.Web.Tests/Controllers/PokemonsControllerTests.cs
@@ -37,7 +37,7 @@ namespace PokeStore.Web.Tests.Controllers
             var mockRepo = new Mock<IPokemonRepository>();
             mockRepo.Setup(repo => repo.GetOne(1)).ReturnsAsync(GetDummyData().FirstOrDefault());
             PokemonsController controller = new PokemonsController(mockRepo.Object);
-            var dummyExpected = new Pokemon { Id = 1, Name = "DummyPokemon", Level = 0, Type = PokemonType.Bug, };
+            Pokemon dummyExpected = new Pokemon { Id = 1, Name = "DummyPokemon", Level = 0, Type = PokemonType.Bug, };
             var expected = JsonConvert.SerializeObject(dummyExpected);
             //When
             var result = await controller.Details(1);
@@ -46,6 +46,36 @@ namespace PokeStore.Web.Tests.Controllers
             var modelResult = Assert.IsAssignableFrom<Pokemon>(viewResult.ViewData.Model);
             var objectResult = JsonConvert.SerializeObject(modelResult);
             Assert.Equal(expected, objectResult);
+        }
+
+        [Fact]
+        public async Task It_Should_Modify_Pokemon_And_Return_To_Index()
+        {
+            //Given
+            var mockRepo = new Mock<IPokemonRepository>();
+            mockRepo.Setup(repo => repo.GetAll()).ReturnsAsync(GetDummyData());
+            PokemonsController controller = new PokemonsController(mockRepo.Object);
+            Pokemon dummy = new Pokemon { Id = 1, Name = "DummyPokemon", Level = 0, Type = PokemonType.Bug, };
+            mockRepo.Setup(repo => repo.Edit(1, dummy)).ReturnsAsync(dummy);
+            //When
+            var result = await controller.EditPokemon(1, dummy);
+            //Then
+            var viewResult = Assert.IsAssignableFrom<RedirectToActionResult>(result);
+            var redirectToAction = (RedirectToActionResult)result;
+            Assert.Equal("Index", redirectToAction.ActionName);
+        }
+
+        [Fact]
+        public async Task It_should_Return_Not_Found_If_Object_Doesnt_Exits()
+        {
+        //Given
+            var mockRepo = new Mock<IPokemonRepository>();
+            mockRepo.Setup(repo => repo.GetOne(1)).ReturnsAsync((Pokemon)null);
+            PokemonsController controller = new PokemonsController(mockRepo.Object);
+        //When
+            var result = await controller.Edit(1);        
+        //Then
+            Assert.IsType<NotFoundResult>(result);
         }
 
         public List<Pokemon> GetDummyData()
